0489a115c7a7eb6a16c8b87acfc009c2427163b9
Pavel Ray <ashd@lowes.com>
2020-04-21 18:49:25 +0530

    Updates

---
+++ precache-manifest.72f4063e4ec7a3b31ddd0fe0917b326b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdae61770cc9ebcb855f65d719dffe2b",
+    "url": "/covid-19-tracker-web/index.html"
+  },
+  {
+    "revision": "6a6f42de3df0b8c3e87a",
+    "url": "/covid-19-tracker-web/static/css/main.64759642.chunk.css"
+  },
+  {
+    "revision": "c8eb252f0b2b36755b28",
+    "url": "/covid-19-tracker-web/static/js/2.8e655605.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker-web/static/js/2.8e655605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6f42de3df0b8c3e87a",
+    "url": "/covid-19-tracker-web/static/js/main.d16f28e6.chunk.js"
+  },
+  {
+    "revision": "182ec94f7da706d2f16c",
+    "url": "/covid-19-tracker-web/static/js/runtime-main.99e5555e.js"
+  }
+]);
